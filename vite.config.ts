9726ae488783e9917ead7e270bcfa2021759f5e3
superwhys <hovenyang.best@gmail.com>
2026-01-16 18:11:35 +0800

    update: path base

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/v2/',
   plugins: [
     vue(),
     AutoImport({
